71516915c0fab0cce2826bfc488b89d38c37c0a3
CerealKiller97 <bogdanovicstefan1997@gmail.com>
2019-08-30 13:40:28 +0200

    TravisCI removed, ZEIT added

--- src/app/modules/shared-components/components/loading-spinner/loading-spinner.component.ts
+++ src/app/modules/shared-components/components/loading-spinner/loading-spinner.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class LoadingSpinnerComponent implements OnInit {
   @Input()
-  loading: boolean;
+  loading = true;
 
   constructor() {}
 
